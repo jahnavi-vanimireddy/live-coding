04b9733afae854d24447bc03f7cde2fc2e8a3b7c
jahnavi-vanimireddy <vanimireddyjahnavi@gmail.com>
2026-09-24 14:22:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function planTrip() {
+    alert(
+        "✈️ Let's plan your perfect trip!\n\n" +
+        "Choose your destination, dates and travel budget."
+    );
+}
+
+function startJourney() {
+    alert(
+        "🌎 Your journey starts here!\n\n" +
+        "Explore destinations and create your travel itinerary."
+    );
+}
